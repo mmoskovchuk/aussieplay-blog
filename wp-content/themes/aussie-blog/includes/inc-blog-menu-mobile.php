11eaca4699e7b2e004dd076d6bcd5a471a48c0c9
Maxim Front End <maxim@domino.local>
2019-11-13 18:31:47 +0200

    added mobile-menu and mobile version main page

---
+++ wp-content/themes/aussie-blog/includes/inc-blog-menu-mobile.php
@@ -0,0 +1,28 @@
+<section class="aussie-casino__blog-menu">
+    <div class="aussie-casino__blog-menu_container">
+        <div class="aussie-casino__blog-menu_wrap-left">
+            <div class="aussie-casino__blog-menu_logo">
+                <a itemprop="url" href="<?php echo home_url(); ?>">
+                    <img itemprop="logo" src="<?php bloginfo('template_url'); ?>/img/logo.svg"
+                         alt="<?php bloginfo('name'); ?>"/>
+                </a>
+            </div>
+            <div class="aussie-casino__blog-menu_url">/ blog</div>
+        </div>
+        <?php
+        $nav_args = array(
+            'menu' => 'blog-menu',
+            'container' => 'nav',
+            'menu_id' => 'blog-menu',
+            'menu_class' => 'aussie-casino__blog-menu_ul-block',
+            'depth' => 4
+        );
+        wp_nav_menu($nav_args);
+        ?>
+
+        <div class="aussie-casino__blog-menu_search"></div>
+        <div id="blog-search" class="aussie-casino__blog-menu_search-btn">
+            <img src="<?php bloginfo('template_url'); ?>/img/search.svg" alt="<?php bloginfo('name'); ?>"/>
+        </div>
+    </div>
+</section>
