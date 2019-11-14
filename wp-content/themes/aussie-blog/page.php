44e592e048c1a8ae0e0d709dd265f511f68599bb
Maxim Front End <maxim@domino.local>
2019-11-14 17:51:53 +0200

    fix mobile version main page

--- wp-content/themes/aussie-blog/page.php
+++ wp-content/themes/aussie-blog/page.php
@@ -1,61 +1,32 @@
 <?php get_header(); ?>
+    <!--HEADER-->
+<?php get_template_part('includes/inc', 'header'); ?>
 
-    <div class="content-wrapper interview-page">
-
-
-    <div class="common-page__common common">
-        <div class="common__content">
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-            <div class="common__header">
-                <div class="common__title"><?php the_title(); ?></div>
-                <div class="common__date">
-                    <?php $date .= the_date('j F Y'); ?>,&nbsp;
-                    <?php the_time('G:i'); ?>
-                    <!--28 января 2018, 6:54-->
+    <div class="common__content">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="common__header">
+                    <div class="common__title"><?php the_title(); ?></div>
+                    <div class="common__date">
+                        <?php $date .= the_date('j F Y'); ?>,&nbsp;
+                        <?php the_time('G:i'); ?>
+                        <!--28 января 2018, 6:54-->
+                    </div>
                 </div>
-            </div>
-            <div class="common__body">
-                <div class="body__text">
+                <div class="common__body">
+                    <div class="body__text">
 
-                                <?php the_content(); ?>
+                        <?php the_content(); ?>
 
+                    </div>
                 </div>
-            </div>
-    <?php endwhile; ?>
-<?php endif; ?>
-        </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 
-        <div class="common-page__footer footer">
-            <div class="footer__source">
-                <?php
-
-                $link = get_field('interview_source');
-
-                if( $link ):
-                    $link_url = $link['url'];
-                    $link_title = $link['title'];
-                    $link_target = $link['target'] ? $link['target'] : '_self';
-                    ?>
-                    <?php pll_e( 'Source' ); ?>
-                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-                <?php endif; ?>
-            </div>
-
-            <div class="footer__share">
-                <div class="share__text"><?php pll_e( 'Share' ); ?></div>
-                <div class="socials">
-                    <a href="javascript:void(0)" class="social share__social">
-                        <div class="social-facebook"></div>
-                    </a>
-                    <a href="javascript:void(0)" class="social share__social">
-                        <div class="icon-share"></div>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-</div>
+    <!--SUBSCRIBE US-->
+<?php get_template_part('includes/inc', 'subscribe-us'); ?>
+    <!--ABOUT US-->
+<?php get_template_part('includes/inc', 'about-us'); ?>
 
 <?php get_footer(); ?>
