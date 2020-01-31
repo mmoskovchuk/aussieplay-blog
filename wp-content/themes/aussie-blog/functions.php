275b02752640da92eb6f1e0735dc53a42080721b
Maxim <maxim@shinningcreation.com>
2020-01-31 09:52:45 +0200

    fix Scheduled Posts

--- wp-content/themes/aussie-blog/functions.php
+++ wp-content/themes/aussie-blog/functions.php
@@ -342,5 +342,19 @@ function filter_function_show_category()
     wp_reset_postdata();
     die();
 }
+// fix Scheduled Posts
+add_filter('the_posts', 'show_all_future_posts');
+
+function show_all_future_posts($posts)
+{
+    global $wp_query, $wpdb;
+
+    if(is_single() && $wp_query->post_count == 0)
+    {
+        $posts = $wpdb->get_results($wp_query->request);
+    }
+
+    return $posts;
+}
 
 
